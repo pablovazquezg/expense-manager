c6f5fa017b60600161b88faac4ccb057ff4dd098
pablovazquezg <pablovazquezg@gmail.com>
2023-06-20 14:25:38 +0000

    feat: improved error logging

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Standard library imports
 import os
 import glob
+import logging
 import pandas as pd
 import concurrent.futures
 
@@ -20,7 +21,9 @@ from src.config import (
     TX_ARCHIVE_FOLDER,
     TX_INPUT_FOLDER,
     TX_OUTPUT_FILE,
-    TX_STAGE_FOLDER
+    TX_STAGE_FOLDER,
+    LOG_FILE,
+    LOG_LEVEL
 )
 
 from src.file_processing import delete_stage_files, merge_files, process_file
@@ -30,12 +33,12 @@ from src.file_processing import delete_stage_files, merge_files, process_file
 #TODO: More sophisticated on the amounts
 
 def main():
-    # Load environment variables from .env file
+    # Set up runnning environment
     load_dotenv()
-
-    # Enable debug mode for langchain
     langchain.debug = True
-
+    logging.basicConfig(filename=LOG_FILE, level=LOG_LEVEL, format='%(asctime)s %(levelname)s %(name)s %(message)s')
+    logger = logging.getLogger(__name__)
+    
     # Get all CSV files in the input folder
     file_paths = glob.glob(os.path.join(TX_INPUT_FOLDER, "*.CSV")) + glob.glob(os.path.join(TX_INPUT_FOLDER, "*.csv"))
 
