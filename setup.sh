16411974531f849606180c460042f864c81c2719
pablovazquezg <pablovazquezg@gmail.com>
2023-06-22 13:56:47 +0000

    Exclude files within data and log folders

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Create data folder and its subfolders
+mkdir -p data/ref_data
+mkdir -p data/tx_data/archive
+mkdir -p data/tx_data/input
+mkdir -p data/tx_data/output
+
+# Create log folder and app.log file
+mkdir -p log
+touch log/app.log
+
+echo "Setup completed successfully!"
